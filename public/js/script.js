9a3e1620006345c1a1f1f7c63e21a67f5d3f806f
anshita <anshitasaini@gmail.com>
2021-08-02 19:41:07 +0000

    current progress on direct messages

--- public/js/script.js
+++ public/js/script.js
@@ -4,8 +4,15 @@ const submitButton = document.querySelector('#send-btn');
 
 const userIdLabel = document.querySelector('#userID');
 const chatSearch = document.querySelector('#chatSearch');
+const chatsBox = document.querySelector('#chatsBox');
+const userToChatWith = document.querySelector('#userToChatWith');
+const send_container = document.querySelector('#send-container');
+const currentChat = document.querySelector('#currentChat');
 
+let msgs = [];
 let db = firebase.database();
+let found;
+let indivchat = false;
 
 let currentUser;  // holds object of user signed in
 
@@ -20,6 +27,7 @@ window.onload = (event) => {
       console.log(currentUser);
       updateUserInfo();  // make sure name & profile pic in db are up to date
       getMessages();
+      loadIndiv();
       // make sure the chat is always at the bottom (latest message) when log in
       // it gives 1 second for all messages to load
       setTimeout(function(){messagesDisplay.scrollTop = messagesDisplay.scrollHeight;}, 1000)
@@ -28,6 +36,32 @@ window.onload = (event) => {
     }
   });
 };
+
+const loadIndiv = () => {
+    db.ref(`users/${currentUser.uid}/chats/`).on('value', (snapshot) => {
+        data = snapshot.val();
+        loadSideButtons(data);
+    })
+}
+
+const loadSideButtons = (data) => {
+    for(key in data) {
+        db.ref(`users/${key}`).get().then((snapshot) => {
+            let userInfo = snapshot.val();
+            let add = `<a class="box chat" href="#" onclick="switchChat('${key}')">
+                                        <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
+                                            <img class="is-rounded" src="${userInfo.profilePic}" alt=""></img>
+                                        </figure>
+                                        <span class="subtitle" style="margin-left: 10px;">${userInfo.displayName}</span>
+                                    </a>`;
+            if (chatsBox.innerHTML.includes(add) == false) {
+                console.log(chatsBox.innerHTML)
+                console.log(add)
+                chatsBox.innerHTML += add
+            }
+        })
+    }
+}
 
 // We will need to store the user info with their chats to allow
 // for making sure the user exists, and also to allow for storing any
@@ -41,6 +75,7 @@ const updateUserInfo = () => {
 };
 
 const getMessages = () => {
+    messagesDisplay.innerHTML = "";
     db.ref(`global_messages/`).on('value', (snapshot) => {
         currentScroll = messagesDisplay.scrollTop;
         let data = snapshot.val();
@@ -48,35 +83,35 @@ const getMessages = () => {
     });
 }
 
+const getIndivMessages = (otherUserID) => {
+    messagesDisplay.innerHTML = "";
+    db.ref(`users/${currentUser.uid}/chats/${otherUserID}/`).on('value', (snapshot) => {
+        currentScroll = messagesDisplay.scrollTop;
+        let data = snapshot.val();
+        renderIndivMessagesAsHtml(data, otherUserID);
+    });
+}
+
 const renderMessagesAsHtml = (data) => {
-    messagesDisplay.innerHTML = ""; // clear all messages before updating to prevent duplicates
-    
     for(key in data){
-        let message = data[key];
-        addMessage(message);
+        if(msgs.includes(key) == false){
+            msgs.push(key);
+            let message = data[key];
+            addMessage(message);
+        }
     }
-    setTimeout(function(){messagesDisplay.scrollTop = currentScroll;}, 200);
 };
 
-// messages will have the most up to date name and profile pic
-// because it gets the info from the DB (which has the updated user info)
-const addMessage = (message) => {
-    db.ref(`users/${message.createdBy}`).get().then((snapshot) => {
-        let userInfo = snapshot.val();
-        let m =`
-            <div class="box sms">
-                <p class="subtitle">${message.message}</p>
-                <div class="sender">
-                    <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
-                        <img class="is-rounded" src="${userInfo.profilePic}" alt="Profile picture of ${userInfo.displayName}"></img>
-                    </figure>
-                    <span id="senderName">By ${userInfo.displayName}</span>
-                </div>
-            </div>
-            `;
-        messagesDisplay.innerHTML += m;
-    });
+const renderIndivMessagesAsHtml = (data, otherUserID) => {
+    for(key in data){
+        if(msgs.includes(key) == false){
+            msgs.push(key);
+            let message = data[key];
+            addMessage(message);
+        }
+    }
 };
+
 
 const submitMessage = () => {
     let m = {
@@ -90,19 +125,115 @@ const submitMessage = () => {
     });
 };
 
-// You can submit the message by pressing enter
-messageInput.addEventListener('keypress', (e) => {
-    if(e.key == "Enter"){
-        submitButton.click();
+const submitIndivMessage = (otherUserID) => {
+    let m = {
+        message: messageInput.value,
+        createdBy: currentUser.uid,
+        createdAt: Date()
     }
-});
+    db.ref(`users/${currentUser.uid}/chats/${otherUserID}/`).push(m).then(() => {
+        messageInput.value = "";
+    });
+    db.ref(`users/${otherUserID}/chats/${currentUser.uid}/`).push(m).then(() => {
+        messageInput.value = "";
+    });
+};
 
+// messages will have the most up to date name and profile pic
+// because it gets the info from the DB (which has the updated user info)
+const addMessage = (message) => {
+    db.ref(`users/${message.createdBy}`).get().then((snapshot) => {
+        let userInfo = snapshot.val();
+        if (message.message !== "") {
+            let m =`
+            <div class="box sms">
+                <p class="subtitle">${message.message}</p>
+                <div class="sender">
+                    <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
+                        <img class="is-rounded" src="${userInfo.profilePic}" alt="Profile picture of ${userInfo.displayName}"></img>
+                    </figure>
+                    <span id="senderName">By ${userInfo.displayName}</span>
+                </div>
+            </div>
+            `;
+            messagesDisplay.innerHTML += m;
+        }
+    });
+};
+
+const addChat = (otherUserID) => {
+    db.ref(`users/${otherUserID}`).get().then((snapshot) => {
+        let userInfo = snapshot.val();
+        db.ref(`users/${currentUser.uid}/chats/${otherUserID}`).push({
+            message: "",
+            createdBy: "",
+            createdAt: ""
+        })
+        db.ref(`users/${otherUserID}/chats/${currentUser.uid}`).push({
+            message: "",
+            createdBy: "",
+            createdAt: ""
+        })
+    })
+}
+
+const switchChat = (otherUserID) => {
+    indivchat = true;
+    send_container.innerHTML = `<button id="send-btn" class="button is-medium is-link is-outlined is-rounded" type="submit"
+                            onclick="submitIndivMessage('${otherUserID}')" style="width:19%">
+                            Submit Indiv
+                        </button>`
+    currentChat.innerHTML = `Chat with ${otherUserID}`
+    getIndivMessages(otherUserID);
+}
+
+const switchGlobal = () => {
+    indivchat = false;
+    send_container.innerHTML = `<button id="send-btn" class="button is-medium is-link is-outlined is-rounded" type="submit"
+                            onclick="submitMessage()" style="width:19%">
+                            Submit
+                        </button>`
+    currentChat.innerHTML = `Global Chat`
+    getMessages();
+}
+
+const findUser = (otherUserID) => {
+    for (var i in id) {
+        if (otherUserID == i) {
+            userToChatWith.value = otherUserID;
+        }
+    }    
+}
+
+const checkUser = (otherUserID) => {
+    id = ''
+    userToChatWith.value = 'none'
+    db.ref(`users/`).on('value', (snapshot) => {
+        id = snapshot.val();
+        findUser(otherUserID); 
+    })
+}
 
 // need to make sure to create the `users/<userID` with user info BEFORE continuing!!!
 const createChat = (otherUserID) => {
-    console.log('Creating chat!!!');
+    checkUser(otherUserID);
+    if (userToChatWith.value == 'none') {
+        alert("Error! User ID not found!");
+    }
+    else {
+        console.log('Creating chat!!!');
+        addChat(otherUserID)
+    }
     chatSearch.value = "";
 };
+
+// You can submit the message by pressing enter
+messageInput.addEventListener('keypress', (e) => {
+    if(e.key == "Enter"){
+        submitMessage()
+        // if (indivchat == false) {submitMessage()}
+    }
+});
 
 chatSearch.addEventListener('keypress', (e) => {
     if(e.key == "Enter"){
