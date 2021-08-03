344d995b86d39baf2e7a287c08e10ddd587565ec
Anshita Saini <anshitasaini@gmail.com>
2021-08-03 10:10:58 -0700

    direct messages fully working

--- public/js/script.js
+++ public/js/script.js
@@ -11,12 +11,16 @@ const currentChat = document.querySelector('#currentChat');
 
 let msgs = [];
 let db = firebase.database();
-let found;
 let indivchat = false;
 
 let currentUser;  // holds object of user signed in
 
 let currentScroll = 0;
+
+
+// iTxWedj31lOgen3iEQiuMfxvgQF3
+// bATCbG4BSOdHC01hkav70esAZe52
+// RrDUNItGMiYBjUOQXxaO558FsEQ2
 
 window.onload = (event) => {
   // Use this to retain user state between html pages.
@@ -27,7 +31,7 @@ window.onload = (event) => {
       console.log(currentUser);
       updateUserInfo();  // make sure name & profile pic in db are up to date
       getMessages();
-      loadIndiv();
+      loadIndiv();  // loads chats
       // make sure the chat is always at the bottom (latest message) when log in
       // it gives 1 second for all messages to load
       setTimeout(function(){messagesDisplay.scrollTop = messagesDisplay.scrollHeight;}, 1000)
@@ -38,29 +42,40 @@ window.onload = (event) => {
 };
 
 const loadIndiv = () => {
-    db.ref(`users/${currentUser.uid}/chats/`).on('value', (snapshot) => {
+    db.ref(`users/${currentUser.uid}/chats`).on('value', (snapshot) => {
+        chatsBox.innerHTML = `
+        <a class="box chat" href="#" onclick="switchGlobal()">
+            <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
+                <img class="is-rounded" src="https://www.freeiconspng.com/thumbs/global-icon/global-icon-13.png" alt="Earth icon"></img>
+            </figure>
+            <span class="subtitle" style="margin-left: 10px;">Global Chat</span>
+        </a>
+        `;
         data = snapshot.val();
-        loadSideButtons(data);
+        for(key in data){
+            loadSideButtons(key)
+        }
     })
 }
 
-const loadSideButtons = (data) => {
-    for(key in data) {
-        db.ref(`users/${key}`).get().then((snapshot) => {
+const loadSideButtons = (otherID) => {
+        db.ref(`users/${otherID}`).get().then((snapshot) => {
+            console.log(otherID) 
             let userInfo = snapshot.val();
-            let add = `<a class="box chat" href="#" onclick="switchChat('${key}')">
-                                        <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
-                                            <img class="is-rounded" src="${userInfo.profilePic}" alt=""></img>
-                                        </figure>
-                                        <span class="subtitle" style="margin-left: 10px;">${userInfo.displayName}</span>
-                                    </a>`;
-            if (chatsBox.innerHTML.includes(add) == false) {
-                console.log(chatsBox.innerHTML)
-                console.log(add)
-                chatsBox.innerHTML += add
+            if(userInfo.displayName != null && userInfo.profilePic != null){
+                let add = `<a class="box chat" href="#" onclick="switchChat('${otherID}')">
+                                            <figure class="image is-24x24 profile-pic" style="display:inline-flex;">
+                                                <img class="is-rounded" src="${userInfo.profilePic}" alt=""></img>
+                                            </figure>
+                                            <span class="subtitle" style="margin-left: 10px;">${userInfo.displayName}</span>
+                                        </a>`;
+                if (chatsBox.innerHTML.includes(add) == false) {
+                    // console.log(chatsBox.innerHTML)
+                    // console.log(add)
+                    chatsBox.innerHTML += add
+                }
             }
-        })
-    }
+        });
 }
 
 // We will need to store the user info with their chats to allow
@@ -76,6 +91,7 @@ const updateUserInfo = () => {
 
 const getMessages = () => {
     messagesDisplay.innerHTML = "";
+    msgs = [];
     db.ref(`global_messages/`).on('value', (snapshot) => {
         currentScroll = messagesDisplay.scrollTop;
         let data = snapshot.val();
@@ -85,10 +101,11 @@ const getMessages = () => {
 
 const getIndivMessages = (otherUserID) => {
     messagesDisplay.innerHTML = "";
-    db.ref(`users/${currentUser.uid}/chats/${otherUserID}/`).on('value', (snapshot) => {
+    msgs = [];
+    db.ref(`users/${currentUser.uid}/chats/${otherUserID}`).on('value', (snapshot) => {
         currentScroll = messagesDisplay.scrollTop;
         let data = snapshot.val();
-        renderIndivMessagesAsHtml(data, otherUserID);
+        renderMessagesAsHtml(data);
     });
 }
 
@@ -101,17 +118,6 @@ const renderMessagesAsHtml = (data) => {
         }
     }
 };
-
-const renderIndivMessagesAsHtml = (data, otherUserID) => {
-    for(key in data){
-        if(msgs.includes(key) == false){
-            msgs.push(key);
-            let message = data[key];
-            addMessage(message);
-        }
-    }
-};
-
 
 const submitMessage = () => {
     let m = {
@@ -197,20 +203,15 @@ const switchGlobal = () => {
     getMessages();
 }
 
-const findUser = (otherUserID) => {
-    for (var i in id) {
-        if (otherUserID == i) {
-            userToChatWith.value = otherUserID;
-        }
-    }    
-}
-
-const checkUser = (otherUserID) => {
-    id = ''
+const checkUser = async(otherUserID) => {
     userToChatWith.value = 'none'
-    db.ref(`users/`).on('value', (snapshot) => {
-        id = snapshot.val();
-        findUser(otherUserID); 
+    await db.ref(`users/`).on('value', (snapshot) => {
+        ids = snapshot.val();
+        for (let i in ids) {
+            if (otherUserID == i) {
+                userToChatWith.value = otherUserID;
+            }
+        } 
     })
 }
 
@@ -228,12 +229,11 @@ const createChat = (otherUserID) => {
 };
 
 // You can submit the message by pressing enter
-messageInput.addEventListener('keypress', (e) => {
-    if(e.key == "Enter"){
-        submitMessage()
-        // if (indivchat == false) {submitMessage()}
-    }
-});
+// messageInput.addEventListener('keypress', (e) => {
+//     if(e.key == "Enter"){
+//         submitMessage();
+//     }
+// });
 
 chatSearch.addEventListener('keypress', (e) => {
     if(e.key == "Enter"){
